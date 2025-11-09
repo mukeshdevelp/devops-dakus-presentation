3d2757ff5e6ad8d015020e479a05520b353f1f44
mukeshdevelp <msmukeshkumarsharma95@gmail.com>
2025-11-10 01:41:27 +0530

    python code aded

--- deploy.py
+++ deploy.py
@@ -1,17 +1,42 @@
-import os
+from flask import Flask, request
+import subprocess
+import json
+
+
+app = Flask(__name__)
 
 IMAGE = "msmukeshkumarsharma/demo-app-npm-v2"
 CONTAINER = "myapp"
 PORT = "3000"
 
-commands = [
-    f"docker pull {IMAGE}:latest",
-    f"docker stop {CONTAINER} || true",
-    f"docker rm {CONTAINER} || true",
-    f"docker run -d --name {CONTAINER} --restart always -p {PORT}:{PORT} {IMAGE}:latest",
-    "docker run -d --name watchtower --restart always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower " + CONTAINER + " --interval 30"
-]
+CONTAINER = "myapp"
+PORT = "3000"
 
-for cmd in commands:
-    print(f"$ {cmd}")
-    os.system(cmd)
+def deploy():
+    run(f"docker pull {IMAGE}:latest")
+    run(f"docker stop {CONTAINER} || true")
+    run(f"docker rm {CONTAINER} || true")
+    run(f"""
+docker run -d \
+  --name {CONTAINER} \
+  --restart always \
+  -p {PORT}:{PORT} \
+  {IMAGE}:latest
+""")
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    try:
+        payload = request.get_json()
+        print("Received Webhook:")
+        print(json.dumps(payload, indent=2))
+    except:
+        print("Received Webhook (no JSON body)")
+
+    print("Deploying new version...")
+    deploy()
+    return "OK", 200
+
+if __name__ == "__main__":
+    print("Listening for Docker Hub webhooks on port 5001 ...")
+    app.run(host="0.0.0.0", port=5001)
